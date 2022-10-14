9db3329e0357c758d6de8173f5eeca9e1da106ff
yeshwanth806 <90232827+yeshwanth806@users.noreply.github.com>
2022-10-14 19:38:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import cv2
 import numpy as np
 import av
 import mediapipe as mp
+import streamlit
 from streamlit_webrtc import webrtc_streamer, WebRtcMode, RTCConfiguration
 
 st.title('AppliedRoots Project')
